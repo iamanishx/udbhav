2c6acee7cd7e93075d27c4b14b44e3da38ad67aa
imanishx <manishbiswal754@gmail.com>
2025-11-01 06:50:29 +0000

    change dto
    Signed-off-by: imanishx <manishbiswal754@gmail.com>

--- apps/server/src/dtos/main.dtos.ts
+++ apps/server/src/dtos/main.dtos.ts
@@ -1,9 +1,5 @@
 import z from "zod";
 
 export const MainDTO = z.object({
-  id: z.string().uuid(),
-  name: z.string().min(1).max(100),
-  mail: z.string().email(),
-  createdAt: z.date(),
-  
+  file: z.instanceof(Uint8Array),
 });
